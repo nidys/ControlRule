1182518b6848d9e2b3d7ba54e57948f45e8e2117
nidys <piotr.niedrygas@gmail.com>
2015-01-28 21:56:25 +0100

    tests

--- src/agh/controlrules/db/ConnectionManager.java
+++ src/agh/controlrules/db/ConnectionManager.java
@@ -8,14 +8,15 @@ import agh.controlrules.utils.Logger;
 
 public class ConnectionManager {
 	private static final String DB_DRIVER = "org.postgresql.Driver";
-	private static final String DB_CONNECTION = "jdbc:postgresql://localhost:5432/control_rules_db";
-	private static final String DB_USER = "controlrules";
-	private static final String DB_PASSWORD = "controlrules";
+	private static final String DB_CONNECTION = "jdbc:postgresql://127.0.0.1:5432/control_rules_db";
+	private static final String DB_USER = "postgres"; //controlrules
+	private static final String DB_PASSWORD = "student"; //controlrules
+//	private static final String DB_USER = "controlrules"; //
+//	private static final String DB_PASSWORD = "controlrules"; //
 
 	public static Connection getDBConnection() {
 		Connection dbConnection = null;
 		try {
-			Logger.error("Connecting to=%s, u=%s, p=%s", DB_CONNECTION, DB_USER, DB_PASSWORD);
 			Class.forName(DB_DRIVER);
 		} catch (ClassNotFoundException e) {
 			System.out.println();
--- src/agh/controlrules/test/SomeTest.java
+++ src/agh/controlrules/test/SomeTest.java
@@ -5,47 +5,39 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
-import agh.controlrules.db.DatabaseManager;
-import agh.controlrules.db.queries.InsertControlArguments;
-import agh.controlrules.db.queries.InsertControlConditions;
-import agh.controlrules.db.queries.InsertControlRules;
-import agh.controlrules.db.queries.SelectControlArguments;
-import agh.controlrules.db.queries.SelectControlConditions;
-import agh.controlrules.db.queries.SelectControlRules;
-import agh.controlrules.db.queries.tables.ControlConditions;
-import agh.controlrules.db.queries.tables.ControlRules;
-import agh.controlrules.db.queries.tables.QueryCreator;
 import agh.controlrules.utils.Logger;
 import agh.db.ControlRuleDaoImpl;
 
 public class SomeTest {
 
+	static String sampleRules[] = new String[] { RulesExmaple.TEST_6, RulesExmaple.TEST_7, RulesExmaple.TEST_off, RulesExmaple.TEST_test };
+
 	public static void main(String[] args) {
-		System.out.println("1 - insert\n2 - select\n3-getall\n4-delete\n5 - quit");
-		// new SomeTest().insertTest1();
-		new SomeTest().selectTest1();
-		// new ControlRuleDaoImpl().deleteControlRule("1a1b");
-		// f();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String userName = null;
-
-		// read the username from the command-line; need to use try/catch with
-		// the
-		// readLine() method
+		String cmd = null;
+		printHelp();
 		try {
-			while ((userName = br.readLine()) != null) {
-				if (userName.equals("1")) {
-					new SomeTest().insertTest1();
-				} else if (userName.equals("2")) {
-					new SomeTest().selectTest1();
-				} else if (userName.equals("3")) {
-					f();
-				} else if (userName.equals("4")) {
-					new ControlRuleDaoImpl().deleteControlRule("1a1b");
-				} else
+			while ((cmd = br.readLine()) != null) {
+				if (isOption(cmd, 0)) {
+					o0();
+				} else if (isOption(cmd, 1)) {
+					o1_insert(br);
+				} else if (cmd.equals("2")) {
+					o2_select(br);
+				} else if (cmd.equals("3")) {
+					o3_delete(br);
+				} else if (cmd.equals("4")) {
+					o4(br);
+				} else if (cmd.equals("5")) {
+					o5(br);
+				} else if (cmd.equals("6")) {
+					o6(br);
+				} else {
+					System.out.println("Good byte!");
 					break;
-				Logger.info("-------------------------------");
+				}
+				printHelp();
 			}
 		} catch (IOException ioe) {
 			System.out.println("IO error trying to read your name!");
@@ -54,132 +46,73 @@ public class SomeTest {
 
 	}
 
-	private static void f() {
+	private static boolean isOption(String cmd, int num) {
+		return cmd.equals(String.valueOf(num));
+	}
+
+	private static void printHelp() {
+		int i = 0;
+		Logger.info("-------------------------------");
+		System.out.println(i++ + " - print all rules in db");
+		System.out.println(i++ + " - insert rule, after enter number of rule");
+		System.out.println(i++ + " - select rule, after enter number of rule");
+		System.out.println(i++ + " - delete rule, after enter number of rule");
+		System.out.println(i++ + " - print available sample rules");
+		System.out.println(i++ + " - set logging level to info");
+		System.out.println(i++ + " - set logging level to error");
+	}
+
+	private static void o0() {
 		List<String> names = new ControlRuleDaoImpl().getAllControlRules();
-		for (String str : names) {
-			Logger.info(str);
+		System.out.println("Got rules names:");
+		for (int i = 0; i < names.size(); i++) {
+			System.out.println(i + ")" + names.get(i));
 		}
+		System.out.println("----------");
 	}
 
-	// Logger.info();
-	public void selectTest1() {
-		Db db = new Db();
-		List<QueryCreator> list = db.select(new SelectControlRules("1a1b"));
-		for (QueryCreator qc : list) {
-			Logger.info(qc.toString());
-			ControlRules cr = (ControlRules) qc;
-			List<QueryCreator> conditions = db.select(new SelectControlConditions(cr.rule_id));
-			for (QueryCreator qcConditions : conditions) {
-				Logger.info("\t" + qcConditions.toString());
-				ControlConditions cc = (ControlConditions) qcConditions;
-				List<QueryCreator> arguments = db.select(new SelectControlArguments(cc.condition_id));
-				for (QueryCreator qcArguments : arguments) {
-					Logger.info("\t\t" + qcArguments.toString());
-				}
-			}
+	private static void o1_insert(BufferedReader br) throws IOException {
+		System.out.print("Enter which rule:");
+		String cmd = br.readLine();
+		int ruleNum = Integer.valueOf(cmd);
+		System.out.println("Inserting:");
+		new ControlRuleDaoImpl().addControlRule(sampleRules[ruleNum]);
+		System.out.println("----------");
+	}
+
+	private static void o2_select(BufferedReader br) throws IOException {
+		System.out.print("Enter which rule:");
+		String cmd = br.readLine();
+		int ruleNum = Integer.valueOf(cmd);
+		System.out.println("Getting rule...");
+		System.out.println("Got:" + new ControlRuleDaoImpl().getControlRule(sampleRules[ruleNum]));
+		System.out.println("----------");
+	}
+
+	private static void o3_delete(BufferedReader br) throws IOException {
+		System.out.print("Enter which rule:");
+		String cmd = br.readLine();
+		int ruleNum = Integer.valueOf(cmd);
+		System.out.println("Deleting");
+		new ControlRuleDaoImpl().deleteControlRule(sampleRules[ruleNum]);
+		System.out.println("----------");
+	}
+
+	private static void o4(BufferedReader br) throws IOException {
+		System.out.println("Rules:");
+		for(int i = 0; i < sampleRules.length; i++) {
+			System.out.println(i + ") -----------------\n" + sampleRules[i]);
 		}
+		System.out.println("----------");
 	}
 
-	public void selectTest1_differentFormat() {
-		Db db = new Db();
-		Logger.level = 1;
-		List<QueryCreator> list = db.select(new SelectControlRules("1a1b"));
-		for (QueryCreator qc : list) {
-			Logger.info(qc.asString());
-			ControlRules cr = (ControlRules) qc;
-			List<QueryCreator> conditions = db.select(new SelectControlConditions(cr.rule_id));
-			for (QueryCreator qcConditions : conditions) {
-				Logger.info("\t" + qcConditions.asString());
-				ControlConditions cc = (ControlConditions) qcConditions;
-				List<QueryCreator> arguments = db.select(new SelectControlArguments(cc.condition_id));
-				for (QueryCreator qcArguments : arguments) {
-					Logger.info("\t\t" + qcArguments.asString());
-				}
-			}
-		}
+	private static void o5(BufferedReader br) throws IOException {
+		System.out.println("Setting logging to info");
+		Logger.level = 5;
 	}
 
-	public void insertTest1() {
-		Logger.info("---test1---");
-		Db db = new Db();
-		int ruleId = db.insert(new InsertControlRules("1a1b", "r"));
-		insertFirstCondition(db, ruleId);
-		insertSecondCondition(db, ruleId);
-		insertThirdCondition(db, ruleId);
-		insertFourthCondition(db, ruleId); // forall
-		insertFifthCondition(db, ruleId);
+	private static void o6(BufferedReader br) throws IOException {
+		System.out.println("Setting logging to error");
+		Logger.level = 2;
 	}
-
-	private void insertFirstCondition(Db db, int ruleId) {
-		int controlCondition1 = db.insert(new InsertControlConditions(ruleId, "v"));
-		db.insert(new InsertControlArguments(controlCondition1, "d=dark"));
-		db.insert(new InsertControlArguments(controlCondition1, "L"));
-		db.insert(new InsertControlArguments(controlCondition1, "detected=false"));
-	}
-
-	private void insertSecondCondition(Db db, int ruleId) {
-		int controlCondition1 = db.insert(new InsertControlConditions(ruleId, "e"));
-		db.insert(new InsertControlArguments(controlCondition1, "d=dark"));
-		db.insert(new InsertControlArguments(controlCondition1, "L"));
-		db.insert(new InsertControlArguments(controlCondition1, "s"));
-		db.insert(new InsertControlArguments(controlCondition1, "J"));
-	}
-
-	private void insertThirdCondition(Db db, int ruleId) {
-		InsertControlConditions query = new InsertControlConditions(ruleId, "e");
-		query.negation = true;
-		int controlCondition1 = db.insert(query);
-		db.insert(new InsertControlArguments(controlCondition1, "d=dark"));
-		db.insert(new InsertControlArguments(controlCondition1, "L"));
-		db.insert(new InsertControlArguments(controlCondition1, "s"));
-		db.insert(new InsertControlArguments(controlCondition1, "J"));
-	}
-
-	private void insertFourthCondition(Db db, int ruleId) {
-		InsertControlConditions forallQuery = new InsertControlConditions(ruleId, "forall");
-		forallQuery.forall = true;
-		int controlCondition1 = db.insert(forallQuery);
-
-		InsertControlConditions queryCondition = new InsertControlConditions(ruleId, "v");
-		queryCondition.reference_condition_action_id = controlCondition1;
-		int controlCondition2 = db.insert(queryCondition);
-		db.insert(new InsertControlArguments(controlCondition2, "d=present"));
-		db.insert(new InsertControlArguments(controlCondition2, "K"));
-
-		InsertControlConditions actionCondition1 = new InsertControlConditions(ruleId, "vla");
-		actionCondition1.reference_condition_action_id = controlCondition1;
-		actionCondition1.action = true;
-		int controlCondition3 = db.insert(actionCondition1);
-		db.insert(new InsertControlArguments(controlCondition3, "d=present"));
-		db.insert(new InsertControlArguments(controlCondition3, "K"));
-		db.insert(new InsertControlArguments(controlCondition3, "detected=false"));
-
-		InsertControlConditions actionCondition2 = new InsertControlConditions(ruleId, "vlc");
-		actionCondition2.reference_condition_action_id = controlCondition1;
-		actionCondition2.action = true;
-		int controlCondition4 = db.insert(actionCondition2);
-		db.insert(new InsertControlArguments(controlCondition4, "d=present"));
-		db.insert(new InsertControlArguments(controlCondition4, "J"));
-		db.insert(new InsertControlArguments(controlCondition4, "detected=true"));
-	}
-
-	private void insertFifthCondition(Db db, int ruleId) {
-		InsertControlConditions query = new InsertControlConditions(ruleId, "vla");
-		query.action = true;
-		int controlCondition1 = db.insert(query);
-		db.insert(new InsertControlArguments(controlCondition1, "s"));
-		db.insert(new InsertControlArguments(controlCondition1, "J"));
-		db.insert(new InsertControlArguments(controlCondition1, "off"));
-	}
-
-	class Db {
-		public int insert(QueryCreator query) {
-			return DatabaseManager.insert(query);
-		}
-
-		public List<QueryCreator> select(QueryCreator query) {
-			return DatabaseManager.select(query);
-		}
-	}
-
 }
